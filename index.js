81c1d8ee2bd44498cb7d84014b201b4b6ee69799
Caleb Bray <caleb.bray@economicmodeling.com>
2018-09-25 16:31:41 -0700

    changes before removing ssh key

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
  */
 
 // Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
+var config = require("./lib/config");
+var fs = require("fs");
+var handlers = require("./lib/handlers");
+var helpers = require("./lib/helpers");
 
 // Instantiate HTTP Server
 var httpServer = http.createServer(function(req, res) {
@@ -23,20 +23,20 @@ httpServer.listen(config.httpPort, function() {
   console.log(`The server running on port ${config.httpPort}`);
 });
 
-// Instantiate HTTP Server
-var httpsServerOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem')
-};
+// // Instantiate HTTPS Server
+// var httpsServerOptions = {
+//   key: fs.readFileSync('./https/key.pem'),
+//   cert: fs.readFileSync('./https/cert.pem')
+// };
 
-var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-  unifiedServer(req, res);
-});
+// var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
+//   unifiedServer(req, res);
+// });
 
 // Start the server
-httpsServer.listen(config.httpsPort, function() {
-  console.log(`The server running on port ${config.httpsPort}`);
-});
+// httpsServer.listen(config.httpsPort, function() {
+//   console.log(`The server running on port ${config.httpsPort}`);
+// });
 
 // All the server logic
 var unifiedServer = function(req, res) {
@@ -45,7 +45,7 @@ var unifiedServer = function(req, res) {
 
   // Get the path
   var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // Get the query string as an object
   var queryStringObject = parsedUrl.query;
@@ -57,17 +57,17 @@ var unifiedServer = function(req, res) {
   var headers = req.headers;
 
   // Get the payload,if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', function(data) {
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
     buffer += decoder.write(data);
   });
-  req.on('end', function() {
+  req.on("end", function() {
     buffer += decoder.end();
 
     // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
     var chosenHandler =
-      typeof router[trimmedPath] !== 'undefined'
+      typeof router[trimmedPath] !== "undefined"
         ? router[trimmedPath]
         : handlers.notFound;
 
@@ -83,19 +83,19 @@ var unifiedServer = function(req, res) {
     // Route the request to the handler specified in the router
     chosenHandler(data, function(statusCode, payload) {
       // Use the status code returned from the handler, or set the default status code to 200
-      statusCode = typeof statusCode == 'number' ? statusCode : 200;
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
 
       // Use the payload returned from the handler, or set the default payload to an empty object
-      payload = typeof payload == 'object' ? payload : {};
+      payload = typeof payload == "object" ? payload : {};
 
       // Convert the payload to a string
       var payloadString = JSON.stringify(payload);
 
       // Return the response
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(statusCode);
       res.end(payloadString);
-      console.log('Returning this response: ', statusCode, payloadString);
+      console.log("Returning this response: ", statusCode, payloadString);
     });
   });
 };
