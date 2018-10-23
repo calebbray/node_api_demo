77904e918201b0ad79f17b20fc92183d9e4163f1
Caleb Bray <cjbray23@gmail.com>
2018-10-23 07:31:06 -0700

    more front end handlers and pages

--- public/app.js
+++ public/app.js
@@ -92,3 +92,217 @@ app.client.request = function(
   var payloadString = JSON.stringify(payload);
   xhr.send(payloadString);
 };
+
+// Bind the forms
+app.bindForms = function() {
+  if (document.querySelector('form')) {
+    document.querySelector('form').addEventListener('submit', function(e) {
+      // Stop it from submitting
+      e.preventDefault();
+      var formId = this.id;
+      var path = this.action;
+      var method = this.method.toUpperCase();
+
+      // Hide the error message (if it's currently shown due to a previous error)
+      document.querySelector('#' + formId + ' .formError').style.display =
+        'hidden';
+
+      // Turn the inputs into a payload
+      var payload = {};
+      var elements = this.elements;
+      for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type !== 'submit') {
+          var valueOfElement =
+            elements[i].type == 'checkbox'
+              ? elements[i].checked
+              : elements[i].value;
+          payload[elements[i].name] = valueOfElement;
+        }
+      }
+
+      // Call the API
+      app.client.request(undefined, path, method, undefined, payload, function(
+        statusCode,
+        responsePayload
+      ) {
+        // Display an error on the form if needed
+        if (statusCode !== 200) {
+          // Try to get the error from the api, or set a default error message
+          var error =
+            typeof responsePayload.Error == 'string'
+              ? responsePayload.Error
+              : 'An error has occured, please try again';
+
+          // Set the formError field with the error text
+          document.querySelector(
+            '#' + formId + ' .formError'
+          ).innerHTML = error;
+
+          // Show (unhide) the form error field on the form
+          document.querySelector('#' + formId + ' .formError').style.display =
+            'block';
+        } else {
+          // If successful, send to form response processor
+          app.formResponseProcessor(formId, payload, responsePayload);
+        }
+      });
+    });
+  }
+};
+
+// Form response Processor
+app.formResponseProcessor = function(formId, requestPayload, responsePayload) {
+  var functionToCall = false;
+  if (formId == 'accountCreate') {
+    // Take the phone and password and use it to login
+    var newPayload = {
+      phone: requestPayload.phone,
+      password: requestPayload.password
+    };
+
+    app.client.request(
+      undefined,
+      'api/token',
+      'POST',
+      undefined,
+      newPayload,
+      function(statusCode, newResponsePayload) {
+        if (newStatusCode !== 200) {
+          // Set the formError field with the error text
+          document.querySelector('#' + formId + ' .formError').innerHTML =
+            'Sorry, an error has occured. Please try again.';
+
+          // Show (unhide) the form error field on the form
+          document.querySelector('#' + formId + ' .formError').style.display =
+            'block';
+        } else {
+          // If successful, set the token and redirect the user
+          app.setSessionToken(newResponsePayload);
+          window.location = '/checks/all';
+        }
+      }
+    );
+  }
+
+  if (formId == 'sessionCreate') {
+    app.setSessionToken(responsePayload);
+    window.location = '/checks/all';
+  }
+};
+
+// Get the session token from localStorage and set it to the config object
+app.getSessionToken = function() {
+  var tokenString = localStorage.getItem('token');
+  if (typeof tokenString == 'string') {
+    try {
+      var token = JSON.parse(tokenString);
+      app.config.sessionToken = token;
+      if (typeof token == 'object') {
+        app.setLoggedInClass(true);
+      } else {
+        app.setLoggedInClass(false);
+      }
+    } catch (e) {
+      app.config.sessionToken = false;
+      app.setLoggedInClass(false);
+    }
+  }
+};
+
+// Set or remove logged in classes to the DOM
+app.setLoggedInClass = function(bool) {
+  var target = document.querySelector('body');
+  if (bool) {
+    target.classList.add('loggedIn');
+  } else {
+    target.classList.remove('loggedIn');
+  }
+};
+
+// Set the session token in the app.config object and persist to local storage
+app.setSessionToken = function(token) {
+  app.config.sessionToken = token;
+  var tokenString = JSON.stringify(token);
+  localStorage.setItem('token', tokenString);
+  if (typeof token == 'object') {
+    app.setLoggedInClass(true);
+  } else {
+    app.setLoggedInClass(false);
+  }
+};
+
+// Renew the current token
+app.renewToken = function(callback) {
+  var currentToken =
+    typeof app.config.sessionToken == 'object'
+      ? app.config.sessionToken
+      : false;
+
+  if (currentToken) {
+    // Update the token with a new expiration
+    var payload = {
+      id: currentToken.id,
+      extend: true
+    };
+
+    app.client.request(
+      undefined,
+      'api/tokens',
+      'PUT',
+      undefined,
+      payload,
+      function(statusCode, responsePayload) {
+        // Display an error on the form if needed
+        if (statusCode == 200) {
+          // Get the new token details
+          var queryStringObject = { id: currentToken.id };
+          app.client.request(
+            undefined,
+            'api/tokens',
+            'GET',
+            queryStringObject,
+            undefined,
+            function(statusCode, responsePayload) {
+              if (statusCode == 200) {
+                app.setSessionToken(responsePayload);
+                callback(false);
+              } else {
+                app.setSessionToken(false);
+                callback(true);
+              }
+            }
+          );
+        } else {
+          app.setSessionToken(false);
+          callback(true);
+        }
+      }
+    );
+  }
+};
+
+// Loop to renew token often
+app.tokenRenewalLoop = function() {
+  setInterval(function() {
+    app.renewToken(function(err) {
+      if (!err) {
+        console.log('Token renewed successfully @ ' + Date.now());
+      }
+    });
+  }, 1000 * 60);
+};
+
+app.init = function() {
+  // Bind all form submissions
+  app.bindForms();
+
+  // Get the token from localstorage
+  app.getSessionToken();
+
+  // Renew token
+  app.tokenRenewalLoop();
+};
+
+window.onload = function() {
+  app.init();
+};
